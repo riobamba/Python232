11ab4d679a119fa04b601127ab00a491fdba1102
riobamba <davidriobamba@gmail.com>
2016-06-11 19:17:46 -0500

    Prueba2.py
    Se conecta al servicio de estaciones y consulta los datos de
    funcionamiento pero es dificil saber que estaciones son las que deben
    mostrarse por lo que le toma mucho tiempo y escribe en un archivo que
    luego no se puede leer

--- prueba2.py
+++ prueba2.py
@@ -4,8 +4,9 @@
 import sys
 import re
 from seiscomp3 import Client, IO, DataModel, Core
-import requests
 import json
+import requests
+
 
 class WfqQuery(Client.Application):
 
@@ -40,41 +41,44 @@ class WfqQuery(Client.Application):
     return True
 
   def run(self):
-    response = requests.get("http://10.100.100.232:8081/query/stations.json")
-
-   # if response.status_code == 200:
-     # results = response.json()
-     # obj=json.read(response)
-   # else:
-      #print "Error code %s" % response.status_code
-
-
     if not self.query():
       print "No database connection"
       return False
-    
-    (net, sta, loc, cha) = self._streamID.split(".")
-    print net+" "+sta+" "+loc+" "+cha
-    print (self._start)
-    print Core.Time.FromString(self._start,"%Y-%m-%d %H:%M:%S")
-    print (self._end)
-    print Core.Time.FromString(self._end,"%Y-%m-%d %H:%M:%S")
-
     #Creacion de un archivo 
     outfile = open('texto.txt', 'w')
+    
+    response = requests.get("http://10.100.100.232:8081/query/stations.json")
+    if response.status_code == 200:
+      results = response.json()
+      for result in results['Inventory']['network']:
+        for station in result['station']:
+          for location in station['sensorLocation']:
+            for channel in location['stream']:
+              print result['code'],station['code'],location['code'],channel['code']
+              net=str(result['code'])
+              sta=str(station['code'])
+              loc=str(location['code'])
+              cha=str(channel['code'])
+              outfile.write(net+" "+sta+" "+loc+" "+cha+"\n")
+             # it = self.query().getWaveformQuality(DataModel.WaveformStreamID(net,sta,loc,cha, ""),
+              #                                               self._parameter,
+               #                                              Core.Time.FromString(self._start,"%Y-%m-%d %H:%M:%S"),
+                #                                             Core.Time.FromString(self._end,"%Y-%m-%d %H:%M:%S"))
+              #while it.get():
+                #se debe realzar el Cast para poder acceder a los atributos de la clase WaveformQuality como .value() mas en  http://www.seiscomp3.org/doc/seattle/2013.274/base/api-python.html#api-python-datamodel-waveformquality
+                #wfq = DataModel.WaveformQuality.Cast(it.get())
+                #print wfq.start() #muestra la fecha de inicio
+                #print wfq.value() #muestra el procentaje
+                #outfile.write(net+" "+sta+" "+loc+" "+cha+" "+str(wfq.value())+"\n")
+               
+                          
+                #it.step()
+    else:
+        print "Error code %s" % response.status_code
 
-    it = self.query().getWaveformQuality(DataModel.WaveformStreamID(net, sta, loc, cha, ""),
-                                         self._parameter,
-                                         Core.Time.FromString(self._start,"%Y-%m-%d %H:%M:%S"),
-                                         Core.Time.FromString(self._end,"%Y-%m-%d %H:%M:%S"))
-    while it.get():
-     #se debe realzar el Cast para poder acceder a los atributos de la clase WaveformQuality como .value() mas en  http://www.seiscomp3.org/doc/seattle/2013.274/base/api-python.html#api-python-datamodel-waveformquality
-      wfq = DataModel.WaveformQuality.Cast(it.get())
-      #print wfq.start() #muestra la fecha de inicio
-      #print wfq.value() #muestra el procentaje
-      outfile.write(net+" "+sta+" "+loc+" "+cha+" "+str(wfq.value())+"\n")
-      
-      it.step()
+    
+
+
 
     outfile.close()
     return True
--- stations.py
+++ stations.py
@@ -1,24 +1,75 @@
+#!/usr/bin/env python
 import json
 import requests
-
-class Stations:
-	"""docstring for Stations """
-	def __init__(self):
-		response = requests.get("http://10.100.100.232:8081/query/stations.json")
-		if response.status_code == 200:
-		   results = response.json()
-		   for result in results['Inventory']['network']:
-		       for station in result['station']:
-			       	for location in station['sensorLocation']:
-			       		for channel in location['stream']:
-			       			print result['code'],station['code'],location['code'],channel['code']
-		else:
-		   print "Error code %s" % response.status_code
+import sys
+import re
+from seiscomp3 import Client, IO, DataModel, Core
 
 
 
+class WfqQuery(Client.Application):
+
+  def __init__(self, argc, argv):
+    Client.Application.__init__(self, argc, argv)
+
+    self.setPrimaryMessagingGroup("LISTENER_GROUP")
+    self.addMessagingSubscription("QC")
+    self.setMessagingEnabled(True)
+    self.setDatabaseEnabled(True, True)
+    self.setAutoApplyNotifierEnabled(False)
+    self.setInterpretNotifierEnabled(True)
+
+    self._start = '2016-06-09 00:00:00'
+    self._end = '2016-06-09 23:00:00'
+    self._outfile = 'xmx.xml'
+    
+    response = requests.get("http://10.100.100.232:8081/query/stations.json")
+    if response.status_code == 200:
+        results = response.json()
+        for result in results['Inventory']['network']:
+          for station in result['station']:
+	          for location in station['sensorLocation']:
+	            for channel in location['stream']:
+	              print result['code'],station['code'],location['code'],channel['code']
+	              self.consultarPorcentaje(result['code'],station['code'],location['code'],channel['code'])
+    else:
+        print "Error code %s" % response.status_code
+
+
+  def createCommandLineDescription(self):
+    self.commandline().addGroup("Query")
+    self.commandline().addStringOption("Query", "startTime,b", "start time: YYYY-MM-DD hh:mm:ss")
+    self.commandline().addStringOption("Query", "endTime,e", "end time: YYYY-MM-DD hh:mm:ss")
+
+    return True
 
 
 
-app = Stations()
-		
+  def consultarPorcentaje(self,net, sta, loc, cha):
+    if not self.query():
+      print "No database connection"
+      return False
+    
+    #Creacion de un archivo 
+    outfile = open('texto.txt', 'w')
+
+    it = self.query().getWaveformQuality(DataModel.WaveformStreamID(net, sta, loc, cha, ""),
+                                         self._parameter,
+                                         Core.Time.FromString(self._start,"%Y-%m-%d %H:%M:%S"),
+                                         Core.Time.FromString(self._end,"%Y-%m-%d %H:%M:%S"))
+    while it.get():
+     #se debe realzar el Cast para poder acceder a los atributos de la clase WaveformQuality como .value() mas en  http://www.seiscomp3.org/doc/seattle/2013.274/base/api-python.html#api-python-datamodel-waveformquality
+      wfq = DataModel.WaveformQuality.Cast(it.get())
+      #print wfq.start() #muestra la fecha de inicio
+      #print wfq.value() #muestra el procentaje
+      outfile.write(net+" "+sta+" "+loc+" "+cha+" "+str(wfq.value())+"\n")
+      
+      it.step()
+
+    outfile.close()
+    return True
+
+
+app = WfqQuery(len(sys.argv), sys.argv)
+sys.exit(app())
+			
