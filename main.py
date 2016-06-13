4319b6f7081c18bd905e367d5d7d08e403fb5013
riobamba <davidriobamba@gmail.com>
2016-06-13 13:20:42 -0500

    main.py
    se realizan cambios para poder llamar a la clase desde otra y asi poder
    ejecutarla con hilos

---
+++ main.py
@@ -0,0 +1,10 @@
+import prueba2 
+import sys
+  
+def incia(mes):
+  print "inicia", mes
+  app=prueba2.WfqQuery(len(sys.argv), sys.argv)
+
+        
+        
+incia('dato')
--- prueba2.py
+++ prueba2.py
@@ -2,43 +2,32 @@
 
 
 import sys
-import re
-from seiscomp3 import Client, IO, DataModel, Core
+from seiscomp3 import Client, DataModel, Core
 import json
-import requests
+
 
 
 class WfqQuery(Client.Application):
 
   def __init__(self, argc, argv):
     Client.Application.__init__(self, argc, argv)
-
     self.setPrimaryMessagingGroup("LISTENER_GROUP")
     self.addMessagingSubscription("QC")
     self.setMessagingEnabled(True)
     self.setDatabaseEnabled(True, True)
     self.setAutoApplyNotifierEnabled(False)
     self.setInterpretNotifierEnabled(True)
+    self.run()
 
 
-  def createCommandLineDescription(self):
-    self.commandline().addGroup("Query")
-    self.commandline().addStringOption("Query", "startTime,b", "start time: YYYY-MM-DD hh:mm:ss")
-    self.commandline().addStringOption("Query", "endTime,e", "end time: YYYY-MM-DD hh:mm:ss")
-
-    return True
-
-
-  def validateParameters(self):
-    self._parameter = "availability"
-    self._start = self.commandline().optionString("startTime")
-    self._end = self.commandline().optionString("endTime")
-
-    return True
 
   def run(self):
+
+    self._start = '2016-06-11 00:00:00'
+    self._end = '2016-06-11 01:00:00'
+    self._parameter= 'availability'
     if not self.query():
-      print "No database connection"
+      print "Sin conexion a la base de datos"
       return False
     #Creacion de un archivo 
     outfile = open('texto.txt', 'w')
@@ -58,6 +47,7 @@ class WfqQuery(Client.Application):
                                                        Core.Time.FromString(self._start,"%Y-%m-%d %H:%M:%S"),
                                                        Core.Time.FromString(self._end,"%Y-%m-%d %H:%M:%S"))
         val=0;
+        
         while it.get():
           #se debe realzar el Cast para poder acceder a los atributos de la clase WaveformQuality como .value() mas en  http://www.seiscomp3.org/doc/seattle/2013.274/base/api-python.html#api-python-datamodel-waveformquality
           wfq = DataModel.WaveformQuality.Cast(it.get())
@@ -70,7 +60,9 @@ class WfqQuery(Client.Application):
                
     outfile.close()
     return True
-
+  
+  def data(self):
+      return "sirve"
 
 app = WfqQuery(len(sys.argv), sys.argv)
 sys.exit(app())
