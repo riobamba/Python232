609a65f78622eea073ddaaae51ca2953930a5838
riobamba <davidriobamba@gmail.com>
2016-08-04 01:58:02 +0000

    Base de datos
    Se crea la base de datos y se establece los tiempos de consulta

--- latencia/cliente.py
+++ latencia/cliente.py
@@ -2,28 +2,38 @@ import requests
 from apscheduler.schedulers.blocking import BlockingScheduler
 from datetime import datetime
 import time
+import sqlite3
 import os
 from apscheduler.schedulers.background import BackgroundScheduler
 
+
 def tick():
+    conn = sqlite3.connect('latency.sqlite')
+    estacion = conn.cursor()
+    cursor = conn.cursor()
+    estacion.execute("select id,estacion from service_estacion")
     print('Tick! The time is: %s' % datetime.now())
-    url="http://10.100.100.232:8081/query/objects.json?QC&last=%s" % (time.strftime("%Y%m%d.%H%M%S", time.localtime()))
+    url = "http://10.100.100.232:8081/query/objects.json?QC&last=%s" % (time.strftime("%Y%m%d.%H%M%S", time.localtime()))
     print url
-    response = requests.get("url")
+    response = requests.get(url)
     if response.status_code == 200:
         results = response.json()
-        for result in results['request']['QC']['list']:
-            ts = result['params']['latency']['timestamp']
-            print result['id'], result['params']['latency']['value'], ts
+        for row in estacion:
+            band=True
+            for result in results['request']['QC']['list']:
+               station = result['id'].split('.')
+               if station[1] == row[1]:
+                  band=False
+                  cursor.execute('''INSERT INTO service_latencia (valor,fecha,estacion_id) VALUES(?,?,?)''', (result['params']['latency']['value'],datetime.now(),row[0]))
+            if band :
+               cursor.execute('''INSERT INTO service_latencia (valor,fecha,estacion_id) VALUES(?,?,?)''', ('-1', datetime.now(), row[0]))
+        conn.commit()
+
     else:
         print "Error code %s" % response.status_code
 
 
-
-def job_function():
-    print("Hello World")
-
 sched = BlockingScheduler()
-sched.add_job(tick, 'interval', minutes=1)
+sched.add_job(tick, 'interval', seconds=150)
 sched.start()
 
